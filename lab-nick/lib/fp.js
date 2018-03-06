38404cf3e6a15d8e07cc442ee7a2704a017ab097
dambergn <dambergn@gmail.com>
2018-03-06 13:06:55 -0800

    forEach, map, and filter functions and tests complete.  Issues with reduce function, no work on test yet.

--- lab-nick/lib/fp.js
+++ lab-nick/lib/fp.js
@@ -1,43 +1,68 @@
-"use strict";
-
-let array = [1, 2, 3, 4];
-let doubler = (n) => {return 2 * n};
-let result = [];
+'use strict';
 
 // forEach
-const forEach = (array, cb) => {
-    for(let i = 0; i < array.length; i++) {
-        let el = array[i]
-        cb(el)
+function forEach(array, cb) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        let input = cb(array[i]);
+        result.push(input);
     };
+    return result;
 };
-console.log(forEach(array, doubler));
+// console.log(forEach([1, 2, 3, 4, 5], num => num * 2));  //[2, 4, 6, 8, 10]
 
 // Map
-const map = (array, cb) => {
-    let results = [];
+function map(array, cb) {
+    let result = [];
     for (let i = 0; i < array.length; i++) {
-        let input = cb(array[i], i, array)
-        results.push(input);
-    }
-    return results;
+        let input = cb(array[i], i, array);
+        result.push(input);
+    };
+    return result;
 };
-map(array, doubler)
-// console.log(map(array, doubler));
+// console.log(map([1, 2, 3, 4, 5], num => num * 2));  //[2, 4, 6, 8, 10]
 
 //Filter
-const filter = (array, cb) => {
-    return (array, cb);
+function filter (array, cb) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        let input = array[i]
+        if (cb(array[i])) {
+            result.push(input);
+        };
+    };
+    return result;
 };
+// console.log(filter([1, 2, 3, 4, 5], num => num % 2 === 0)); //[2, 4]
 
+//add
+function add (valA, valB) {
+    if (Number.isInteger(valA + valB)) {
+        return (valA + valB);
+      };
+      return null;
+};
 
 //Reduce
-const reduce = (array, cb) => {
-    return (array, cb);
+function reduce (array, cb, start) {
+    let result = start;
+    console.log(result);
+    // array.forEach(item => {
+    //     result = cb.call(undefined, result, item, array);
+    // });
+    for (let i = 0; i < array.length; i++) {
+        result = cb(result, array[i]);
+    }
+    return result;
 };
+// console.log(reduce([1, 2, 3, 4, 5]), add(valA, valB), 0); //[15]
+console.log(reduce([1, 2, 3, 4, 5]), (a, b) => a + b, 0); //[15]
+
+
 
 module.exports = {};
 module.exports.forEach = forEach;
 module.exports.map = map;
 module.exports.filter = filter;
 module.exports.reduce = reduce;
+module.exports.add = add;
--- lab-nick/test/fp.test.js
+++ lab-nick/test/fp.test.js
@@ -1,11 +1,28 @@
 "use strict";
 
-const add = require("../lib/fp.js").forEach;
-const add = require("../lib/fp.js").map;
-const add = require("../lib/fp.js").filter;
-const add = require("../lib/fp.js").reduce;
+const forEach = require("../lib/fp.js").forEach;
+const map = require("../lib/fp.js").map;
+const filter = require("../lib/fp.js").filter;
+const reduce = require("../lib/fp.js").reduce;
+const add = require("../lib/fp.js").add;
 
+describe('forEach Function', () => {
+    it('returns an array with values doubled', () => {
+        expect(forEach([1, 2, 3, 4, 5], num => num * 2)).toEqual([2, 4, 6, 8, 10]);//to equal required for array
+    });
+});
 
+describe('Map Function', () => {
+    it('returns an array with values doubled', () => {
+        expect(map([1, 2, 3, 4, 5], num => num * 2)).toEqual([2, 4, 6, 8, 10]);
+    });
+});
+
+describe('Filter Function', () => {
+    it('returns an array with only even numbers', () => {
+        expect(filter([1, 2, 3, 4, 5], num => num % 2 === 0)).toEqual([2, 4]);
+    });
+});
 
 // describe("add funtion", () => {
 //     it("adds two numbers", () => {
