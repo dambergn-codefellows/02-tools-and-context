fdd188011da8cc0ab0ad62ad05612ccd0095163e
dambergn <dambergn@gmail.com>
2018-03-11 22:09:31 -0700

    comitting to push.

--- lab-nick/test/fp.test.js
+++ lab-nick/test/fp.test.js
@@ -4,7 +4,6 @@ const forEach = require("../lib/fp.js").forEach;
 const map = require("../lib/fp.js").map;
 const filter = require("../lib/fp.js").filter;
 const reduce = require("../lib/fp.js").reduce;
-const add = require("../lib/fp.js").add;
 
 describe('forEach Function', () => {
     it('returns an array with values doubled', () => {
@@ -21,5 +20,11 @@ describe('Map Function', () => {
 describe('Filter Function', () => {
     it('returns an array with only even numbers', () => {
         expect(filter([1, 2, 3, 4, 5], num => num % 2 === 0)).toEqual([2, 4]);
+    });
+});
+
+describe('Reduce Function', () => {
+    it('returns an array with only even numbers', () => {
+        expect(reduce([1, 2, 3, 4, 5], (a, b) => a + b, 0)).toEqual(15);
     });
 });
